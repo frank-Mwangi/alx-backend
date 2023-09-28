cc958841e9d09838aeeef3e4fc51d3e3b28926df
frank-Mwangi <kungofrank@gmail.com>
2023-09-28 10:57:11 +0300

    Start a Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,26 @@
+import { createClient, print } from "redis";
+
+const client = createClient({
+  host: "localhost",
+  port: 6379,
+});
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+client.on("error", (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const ourChannel = "holberton school channel";
+client.subscribe(ourChannel);
+
+client.on("message", (channel, message) => {
+  if (channel === ourChannel) {
+    console.log(message);
+    if (message === "KILL_SERVER") {
+      client.unsubscribe();
+      client.quit();
+    }
+  }
+});
